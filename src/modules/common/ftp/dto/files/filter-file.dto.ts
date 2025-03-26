9e2a5de90ad574c7ef3895b5318bb0863f319530
Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2025-03-26 16:21:23 -0500

    fix bugs

--- src/modules/common/ftp/dto/files/filter-file.dto.ts
+++
@@ -1,5 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-import { PaginationDto } from '@shared/dto';
-import { isNotEmptyValidationOptions } from '@shared/dto-validation';
-
-export class FilterFileDto extends PaginationDto {}
--- src/modules/common/ftp/dto/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './files/filter-file.dto';
-export * from './files/create-file.dto';
-export * from './files/update-file.dto';
