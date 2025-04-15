5f86e5b0bf04666abb73711ea8dc0e1b6dafa59c
Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2025-04-15 16:07:10 -0500

    fix bugs

--- src/modules/common/mail/mail.service.ts
+++ src/modules/common/mail/mail.service.ts
@@ -105,8 +105,7 @@ export class MailService {
 
     const sendMailOptions = {
       to: mailData.to,
-      // from: `${this.configService.mail.fromName} - ${this.configService.mail.from}`,
-      from: `${this.configService.mail.fromName}`,
+      from: `${this.configService.mail.fromName} - ${this.configService.mail.from}`,
       subject: mailData.subject,
       template: mailData.template,
       context: { system: 'environments.appName', data: mailData.data },
