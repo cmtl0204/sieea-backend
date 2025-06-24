2564349f75f8f86be3e4d61c46640d5df4fab994
Cesar Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2025-06-24 10:33:39 -0500

    fix bugs

--- src/modules/core/state/state.service.ts
+++ src/modules/core/state/state.service.ts
@@ -62,7 +62,9 @@ export class StateService {
         entity = this.repository.create();
       }
 
-      const user = await this.userRepository.findOneBy({ identification: cedula });
+      const user = await this.userRepository.findOneBy({
+        identification: cedula,
+      });
 
       if (user) entity.user = user;
 
@@ -72,54 +74,79 @@ export class StateService {
       entity.metaB2 = '100';
       entity.metaC1 = '20';
       entity.metaC2 = '0';
-      entity.habilitado = row
-        .getCell(headerMap['habilitado'])
-        .value?.toString();
-      entity.direccionZonal = row
+
+      if (cedula) entity.cedula = cedula;
+
+      const habilitado = row.getCell(headerMap['habilitado']).value?.toString();
+      if (habilitado) entity.habilitado = habilitado;
+
+      const direccionZonal = row
         .getCell(headerMap['direccionZonal'])
         .value?.toString();
-      entity.provincia = row.getCell(headerMap['provincia']).value?.toString();
-      entity.cedula = row.getCell(headerMap['cedula']).value?.toString();
-      entity.nombres = row.getCell(headerMap['nombres']).value?.toString();
-      entity.actividad = row.getCell(headerMap['actividad']).value?.toString();
-      entity.curso = row.getCell(headerMap['curso']).value?.toString();
-      entity.actividadA1 = row
+      if (direccionZonal) entity.direccionZonal = direccionZonal;
+
+      const provincia = row.getCell(headerMap['provincia']).value?.toString();
+      if (provincia) entity.provincia = provincia;
+
+      const nombres = row.getCell(headerMap['nombres']).value?.toString();
+      if (nombres) entity.nombres = nombres;
+
+      const actividad = row.getCell(headerMap['actividad']).value?.toString();
+      if (actividad) entity.actividad = actividad;
+
+      const curso = row.getCell(headerMap['curso']).value?.toString();
+      if (curso) entity.curso = curso;
+
+      const actividadA1 = row
         .getCell(headerMap['actividadA1'])
         .value?.toString();
-      entity.actividadA2 = row
+      if (actividadA1) entity.actividadA1 = actividadA1;
+
+      const actividadA2 = row
         .getCell(headerMap['actividadA2'])
         .value?.toString();
-      entity.actividadB1 = row
+      if (actividadA2) entity.actividadA2 = actividadA2;
+
+      const actividadB1 = row
         .getCell(headerMap['actividadB1'])
         .value?.toString();
-      entity.actividadB2 = row
+      if (actividadB1) entity.actividadB1 = actividadB1;
+
+      const actividadB2 = row
         .getCell(headerMap['actividadB2'])
         .value?.toString();
-      entity.cantidadA1 = row
-        .getCell(headerMap['cantidadA1'])
-        .value?.toString();
-      entity.cantidadA2 = row
-        .getCell(headerMap['cantidadA2'])
-        .value?.toString();
-      entity.cantidadB1 = row
-        .getCell(headerMap['cantidadB1'])
-        .value?.toString();
-      entity.cantidadB2 = row
-        .getCell(headerMap['cantidadB2'])
-        .value?.toString();
-      entity.cantidadC1 = row
-        .getCell(headerMap['cantidadC1'])
-        .value?.toString();
-      entity.cantidadC2 = row
-        .getCell(headerMap['cantidadC2'])
-        .value?.toString();
-      entity.cumple = row.getCell(headerMap['cumple']).value?.toString();
-      entity.totalCantidadAbril = row
+      if (actividadB2) entity.actividadB2 = actividadB2;
+
+      const cantidadA1 = row.getCell(headerMap['cantidadA1']).value?.toString();
+      if (cantidadA1) entity.cantidadA1 = cantidadA1;
+
+      const cantidadA2 = row.getCell(headerMap['cantidadA2']).value?.toString();
+      if (cantidadA2) entity.cantidadA2 = cantidadA2;
+
+      const cantidadB1 = row.getCell(headerMap['cantidadB1']).value?.toString();
+      if (cantidadB1) entity.cantidadB1 = cantidadB1;
+
+      const cantidadB2 = row.getCell(headerMap['cantidadB2']).value?.toString();
+      if (cantidadB2) entity.cantidadB2 = cantidadB2;
+
+      const cantidadC1 = row.getCell(headerMap['cantidadC1']).value?.toString();
+      if (cantidadC1) entity.cantidadC1 = cantidadC1;
+
+      const cantidadC2 = row.getCell(headerMap['cantidadC2']).value?.toString();
+      if (cantidadC2) entity.cantidadC2 = cantidadC2;
+
+      const cumple = row.getCell(headerMap['cumple']).value?.toString();
+      if (cumple) entity.cumple = cumple;
+
+      const totalCantidadAbril = row
         .getCell(headerMap['totalCantidadAbril'])
         .value?.toString();
-      entity.totalCantidadMayo = row
+      if (totalCantidadAbril) entity.totalCantidadAbril = totalCantidadAbril;
+
+      const totalCantidadMayo = row
         .getCell(headerMap['totalCantidadMayo'])
         .value?.toString();
+      if (totalCantidadMayo) entity.totalCantidadMayo = totalCantidadMayo;
 
       await this.repository.save(entity);
     }
